9e0d73da257b34bf56344da3c2cf7d15bf8bf40b
KapilNITW <123889602+KapilNITW@users.noreply.github.com>
2024-11-29 18:44:02 +0530

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -57,7 +57,13 @@ const CartPage = () => {
                             <div className="row mb-2 p-3 card flex-row">
                                 <div className="col-md-4">
                                     <img
-                                        src={`/api/v1/product/product-photo/${p._id}`}
+                                        src={` {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
+                <img
+                  src={`https://ecommerce-server-93e9.onrender.com/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />/api/v1/product/product-photo/${p._id}`}
                                         className="card-img-top"
                                         alt={p.name}
                                         width="100px"
